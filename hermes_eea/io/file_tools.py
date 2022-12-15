04cb3fff1c0a7f7de730ed6f632f7b400d93457e
Liam Greenlee <liam.a.greenlee@nasa.gov>
2022-12-15 10:57:18 -0500

    Add accum packet unbinning functions

--- hermes_eea/io/file_tools.py
+++ hermes_eea/io/file_tools.py
@@ -1,3 +1,5 @@
+from ccsdspy import FixedLength
+import numpy as np
 """
 This module provides a generic file reader.
 """
@@ -22,3 +24,51 @@ def read_file(data_filename):
     --------
     """
     return None
+
+
+def read_ccsds(filename: str, pkt_def: FixedLength):
+    """
+    Read a ccsds packet file.
+
+    Parameters
+    ----------
+    filename: str
+        A file to read.
+
+    pkt_def: `ccsdspy.FixedLength`
+        CCSDS packet definition
+
+    Returns
+    -------
+    `OrderedDict` mapping field names to NumPy arrays.
+    """
+    result = pkt_def.load(filename)
+    return result
+
+
+def unbin_field(field_bin: np.bytes_, bin_cnt: int, bin_size: int):
+    """
+    Unbin arrays from a packet field.
+
+    Parameters
+    ----------
+    field_bin: `np.bytes_`
+        bytes representation of the field to unbin.
+
+    bin_cnt:
+        Number of bins in the field.
+    
+    bin_size:
+        Length of each bin in the field.
+
+    Returns
+    -------
+    list: List of ints in the array field.
+    """
+    mask = 2**bin_size - 1
+    unbinned = np.zeros(bin_cnt, dtype=int)
+    for bin in range(bin_cnt):
+        shift_size = bin_size * (bin_cnt - bin) - bin_size
+        bin_data = (int.from_bytes(field_bin, "big") >> shift_size) & mask
+        unbinned[bin] = bin_data
+    return unbinned
