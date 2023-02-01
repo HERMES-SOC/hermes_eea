338ece0250615c082c4574fa93a70898b1140b03
Liam Greenlee <liam.a.greenlee@nasa.gov>
2023-02-01 11:31:35 -0500

    Remove unbinning to use ccsdspy array type instead

--- hermes_eea/io/file_tools.py
+++ hermes_eea/io/file_tools.py
@@ -45,31 +45,3 @@ def read_ccsds(filename: str, pkt_def: FixedLength):
     """
     result = pkt_def.load(filename)
     return result
-
-
-def unbin_field(field_bin: np.bytes_, bin_cnt: int, bin_size: int):
-    """
-    Unbin arrays from a packet field.
-
-    Parameters
-    ----------
-    field_bin: `np.bytes_`
-        bytes representation of the field to unbin.
-
-    bin_cnt:
-        Number of bins in the field.
-
-    bin_size:
-        Length of each bin in the field.
-
-    Returns
-    -------
-    list: List of ints in the array field.
-    """
-    mask = 2**bin_size - 1
-    unbinned = np.zeros(bin_cnt, dtype=int)
-    for bin in range(bin_cnt):
-        shift_size = bin_size * (bin_cnt - bin) - bin_size
-        bin_data = (int.from_bytes(field_bin, "big") >> shift_size) & mask
-        unbinned[bin] = bin_data
-    return unbinned
