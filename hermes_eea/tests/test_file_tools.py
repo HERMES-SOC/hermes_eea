f2e09fd37ae802ba32a2b6ca9173e741608e72d2
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2023-12-04 19:16:42 +0000

    Update Tests

--- hermes_eea/tests/test_file_tools.py
+++ hermes_eea/tests/test_file_tools.py
@@ -1,6 +1,8 @@
 import pytest
+import os.path
 from pathlib import Path
 
+import hermes_eea
 from hermes_eea.io import load_data_file, read_calibration_file, write_data_file
 
 level0_filename = "hermes_EEA_l0_2022339-000000_v0.bin"
@@ -27,10 +29,8 @@ def level1_file(tmp_path_factory):
 def test_load_data_file(level0_file):
     with pytest.raises(FileNotFoundError) as excinfo:
         _ = load_data_file(level0_file)
-    assert (
-        str(excinfo.value)
-        == "[Errno 2] No such file or directory: '/workspaces/HERMES_SOC/hermes_eea/hermes_eea/data/EEA_sci_packet_def.csv'"
-    )
+    csv_path = os.path.join(hermes_eea._data_directory, "EEA_sci_packet_def.csv")
+    assert str(excinfo.value) == f"[Errno 2] No such file or directory: '{csv_path}'"
 
 
 def test_read_calibration_file():
