7136e7e085e6802112f1e59bf4377ffb24ab97ec
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2024-01-11 18:44:42 +0000

    Update File Processing for Calibration Test

--- hermes_eea/calibration/calibration.py
+++ hermes_eea/calibration/calibration.py
@@ -49,11 +49,14 @@ def process_file(data_filename: Path) -> list:
     """
     log.info(f"Processing file {data_filename}.")
     output_files = []
-    for filename in data_filename:
-        calibrated_file = calibrate_file(filename)
-        output_files.append(calibrated_file)
-        #  data_plot_files = plot_file(data_filename)
-        #  calib_plot_files = plot_file(calibrated_file)
+    
+    # Calibrate the Input File
+    calibrated_file = calibrate_file(filename)
+    output_files.append(calibrated_file)
+    
+    # Add Plots to the Output Files if we want
+    #  data_plot_files = plot_file(data_filename)
+    #  calib_plot_files = plot_file(calibrated_file)
 
     # add other tasks below
     return output_files
