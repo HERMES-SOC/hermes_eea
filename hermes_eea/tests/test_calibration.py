ae7a590cc94be42dbecea6a3d7a133f001a4a3e3
Damian Barrous-Dume <damianbarrous@gmail.com>
2024-02-02 10:48:28 +0000

    Attempt Windows fix for tests

--- hermes_eea/tests/test_calibration.py
+++ hermes_eea/tests/test_calibration.py
@@ -3,6 +3,7 @@ import os.path
 from pathlib import Path
 import shutil
 import tempfile
+import time
 import ccsdspy
 import hermes_eea
 from hermes_eea.io import read_ccsds
@@ -45,13 +46,30 @@ def test_process_file(small_level0_file):
         A Custom EEA SkymapFactory
         HermesData
     """
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # Create a Temp Copy of the Original
-        temp_test_file_path = Path(tmpdirname, small_level0_file.name)
-        shutil.copy(small_level0_file, temp_test_file_path)
-        # Process the File
-        output_files = calib.process_file(temp_test_file_path)
+    try:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # Create a Temp Copy of the Original
+            temp_test_file_path = Path(tmpdirname, small_level0_file.name)
+            shutil.copy(small_level0_file, temp_test_file_path)
+            # Process the File
+            output_files = calib.process_file(temp_test_file_path)
 
-        assert os.path.getsize(output_files[0]) > 275000
-        cdf = pycdf.CDF(output_files[0])
-        assert len(cdf["Epoch"][:]) == 18
+            assert os.path.getsize(output_files[0]) > 275000
+
+            # Ensure the file is closed before attempting to delete it
+            with pycdf.CDF(output_files[0]) as cdf:
+                assert len(cdf["Epoch"][:]) == 18
+
+    # Ensure the temporary directory is cleaned up even if an exception is raised (needed for Windows)
+    except PermissionError:
+        print("Encountered a PermissionError, retrying file deletion...")
+        time.sleep(0.5)  # Wait a bit for the OS to release any locks
+        cleanup_retry(tmpdirname)
+
+
+def cleanup_retry(directory):
+    """Attempt to clean up the directory after a short delay."""
+    try:
+        shutil.rmtree(directory)
+    except PermissionError as e:
+        print(f"Failed to clean up directory {directory} due to PermissionError: {e}")
